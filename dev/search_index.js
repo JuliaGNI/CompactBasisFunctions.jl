eb78efb3619a65278adbcbab2b8ec51deaf2d99e
Documenter.jl <documenter@juliadocs.github.io>
2020-12-08 18:40:28 +0000

    build based on e3a58bd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CompactBasisFunctions","category":"page"},{"location":"#CompactBasisFunctions","page":"Home","title":"CompactBasisFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CompactBasisFunctions]","category":"page"},{"location":"#CompactBasisFunctions.Lagrange","page":"Home","title":"CompactBasisFunctions.Lagrange","text":"Lagrange basis on the interval [0..1].\n\n\n\n\n\n","category":"type"}]
+}
